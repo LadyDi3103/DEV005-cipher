b5ffc0ed8d7530dd02e7dfdbfb2ec4ccc3d3625c
LadyDi3103 <chinamy2004@gmail.com>
2023-02-17 16:02:10 -0500

    Logramos que nos consolee el textarea y el imput type number y capturamos los valores

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,34 @@
 //Aquí se implementará el objeto cipher
+//Este objeto (cipher) debe contener dos métodos:
 const cipher = {
-  // ...
+  //metodo 1
+
+  //offset es el número de posiciones que queremos mover a la derecha en el alfabeto
+
+  //string el mensaje (texto) que queremos cifrar.
+
+  encode : function encode(offset, string){
+        let alfabeto = "A-Z";
+        string = "";
+          for (let i = 0; i < string.length ; i++) {
+                string.charCodeAt[i]-65 + offset % 26 + 65;
+                return(String.fromCharCode[i]);
+             }         
+console.log(string);
+  },
+
+  //método2
+
+  //offset es el número de posiciones que queremos mover a la izquierda en el alfabeto
+
+  //string el mensaje (texto) que queremos descifrar.
+
+  decode : function decode(offset, string){
+    for (let i = 0; i < string.length; i++) {
+      string.charCodeAt[i]-65 - offset % 26 + 65;
+      return(String.fromCharCode[i]);
+   }
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,66 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
-
-let nombreUser = prompt("¿Cúal es tu nombre?");
-let nombreMayuscula = nombreUser.toUpperCase();
-document.getElementById('nombreUsuario').textContent = nombreMayuscula;
+//Aquí le pedimos al usuario que ingrese su nombre y lo guardamos 
+//en una variable
+// let nombreUser = prompt("¿Cúal es tu nombre?");
+// //Poner el nombre ingresado en Mayúsculas para que resalte
+// let nombreMayuscula = nombreUser.toUpperCase();
+// //Accedemos al DOM y jalamos por ID la etiqueta span y 
+// //su contenido vacio lo reemplamos por el contenido de 
+// document.getElementById('nombreUsuario').textContent = nombreMayuscula;
 
 const seccion0 = document.getElementById('section0');
 const seccion1 = document.getElementById('section1');
 
 const botonInicio = document.getElementById('btnEmpezar');
-console.log(botonInicio);
-
 botonInicio.addEventListener('click', muestraSeccionOne);
 
 function muestraSeccionOne(){
     seccion0.style.display = "none";
     seccion1.style.display = "block";  
 }
+// ************************************************
+
+
+// posiciones.addEventListener('change', () => {
+//     console.log(posiciones.value);
+// })
+// let positions = posiciones.value;
+// console.log("Nueva variable de offset", positions);
+
+let boton = document.getElementById('btnCodificar');
+    boton.addEventListener("click", capturarTextTarea1Yposiciones);
+    
+  
+function capturarTextTarea1Yposiciones(){
+ let posiciones = document.getElementById('positions').value;
+ let texTarea1 = document.getElementById('aCifrar').value;
+ console.log(posiciones);
+ console.log(texTarea1);
+}
+
+let msjCifrado = document.getElementById('msjCifrado');
+msjCifrado.textContent = boton;
+console.log(msjCifrado);
+
+
+
+// boton.addEventListener('click', function());
+
+
+
+// let btnCodificar = document.getElementById('btnCodificar').addEventListener("click", cipher.encode(posiciones,texTarea1)
+
+// msjCifrado
+// cipher.encode(posiciones, texTarea1);
+// //.value
+
+// );
+
+// 
+
+
+// let texTarea2 = document.getElementById('aCifrar1').value;
+// let mensajeDecifrado= cipher.decode(-posiciones, texTarea2);
+// document.getElementById('msjDecifrado').textContent = mensajeDecifrado;
